39e2825e0db33214588eac6e2656c8abca01d356
NatBat92 <natalie.neale1992@outlook.com>
2023-07-10 09:40:05 +0000

    add sign up form and add styles to buttons and sign up form

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://gw2forumapi-7625ca6fa938.herokuapp.com/'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true;
