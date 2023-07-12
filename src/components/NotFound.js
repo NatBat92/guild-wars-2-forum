73294987aabe7108a1f3a19c3713116a780290f9
NatBat92 <natalie.neale1992@outlook.com>
2023-07-12 10:53:43 +0000

    made change to navbar so menu choices change dependant on if you are logged in or not

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,17 +1,76 @@
 import React from "react";
-import NoResults from "../assets/no-results.png";
-import styles from "../styles/NotFound.module.css";
-import Asset from "./Asset";
+import Dropdown from "react-bootstrap/Dropdown";
+import styles from "../styles/MoreDropdown.module.css";
+import { useHistory } from "react-router";
 
-const NotFound = () => {
+// The forwardRef is important!!
+// Dropdown needs access to the DOM node in order to position the Menu
+const ThreeDots = React.forwardRef(({ onClick }, ref) => (
+  <i
+    className="fas fa-ellipsis-v"
+    ref={ref}
+    onClick={(e) => {
+      e.preventDefault();
+      onClick(e);
+    }}
+  />
+));
+
+export const MoreDropdown = ({ handleEdit, handleDelete }) => {
   return (
-    <div className={styles.NotFound}>
-      <Asset
-        src={NoResults}
-        message={`Sorry, the page you're looking for doesn't exist`}
-      />
-    </div>
+    <Dropdown className="ml-auto" drop="left">
+      <Dropdown.Toggle as={ThreeDots} />
+
+      <Dropdown.Menu
+        className="text-center"
+        popperConfig={{ strategy: "fixed" }}
+      >
+        <Dropdown.Item
+          className={styles.DropdownItem}
+          onClick={handleEdit}
+          aria-label="edit"
+        >
+          <i className="fas fa-edit" />
+        </Dropdown.Item>
+        <Dropdown.Item
+          className={styles.DropdownItem}
+          onClick={handleDelete}
+          aria-label="delete"
+        >
+          <i className="fas fa-trash-alt" />
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
   );
 };
 
-export default NotFound;
+export const ProfileEditDropdown = ({ id }) => {
+  const history = useHistory();
+  return (
+    <Dropdown className={`ml-auto px-3 ${styles.Absolute}`} drop="left">
+      <Dropdown.Toggle as={ThreeDots} />
+      <Dropdown.Menu>
+        <Dropdown.Item
+          onClick={() => history.push(`/profiles/${id}/edit`)}
+          aria-label="edit-profile"
+        >
+          <i className="fas fa-edit" /> edit profile
+        </Dropdown.Item>
+        <Dropdown.Item
+          onClick={() => history.push(`/profiles/${id}/edit/username`)}
+          aria-label="edit-username"
+        >
+          <i className="far fa-id-card" />
+          change username
+        </Dropdown.Item>
+        <Dropdown.Item
+          onClick={() => history.push(`/profiles/${id}/edit/password`)}
+          aria-label="edit-password"
+        >
+          <i className="fas fa-key" />
+          change password
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
